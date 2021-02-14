572e622d867b551e47a2a91a8372222c170779c7
lin-ister <bplin@ucsd.edu>
2021-02-13 21:57:23 -0800

    "need working"

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,25 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+let ul = document.getElementById('product-list');
+const url = 'https://fakestoreapi.com/products';
+
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  localStorage.setItem('products', JSON.stringify(data));
+  let prods = JSON.parse(localStorage.getItem('products'));
+  return prods.map(function(prod){
+    let img = prod.image;
+    let title = prod.title;
+    let num = prod.price;
+    let price = num.toString();
+    let consume = new ProductItem(img, title, price);
+    console.log(consume);
+    ul.appendChild(consume);
+  })
+})
+
 });
+
